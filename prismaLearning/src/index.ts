73a26248d4d24bdfc18d814b9f2da333876f1fed
Thalisu <thalisondmcc@gmail.com>
2024-07-25 19:37:38 -0300

    Create many

--- prismaLearning/src/index.ts
+++ prismaLearning/src/index.ts
@@ -3,7 +3,14 @@ import { StoreServices } from "./services/store.service";
 const storeServices = new StoreServices();
 
 (async () => {
-  const response = await storeServices.create("Thalisu Store");
+  /*   const response = await storeServices.create("Thalisu Store");
+
+  console.log(response); */
+
+  const response = await storeServices.createMany([
+    "Carina Store",
+    "Uneve Store",
+  ]);
 
   console.log(response);
 })();
--- prismaLearning/src/services/store.service.ts
+++ prismaLearning/src/services/store.service.ts
@@ -4,4 +4,10 @@ export class StoreServices {
   async create(storeName: string) {
     return await prisma.store.create({ data: { name: storeName } });
   }
+
+  async createMany(storeNames: string[]) {
+    const stores = storeNames.map((storeName) => ({ name: storeName }));
+
+    return await prisma.store.createMany({ data: stores });
+  }
 }
