68018fe1c6d8766fa8deb040d1e88adcb895cb6a
Thalisu <thalisondmcc@gmail.com>
2024-09-16 14:31:18 -0300

    many serialize field

--- python/tags/serializers.py
+++ python/tags/serializers.py
@@ -5,4 +5,4 @@ from .models import Tag
 class TagSerialier(serializers.Serializer):
     name = serializers.CharField(max_length=30, choices=Tag.choices, default=Tag.OTHER)  # type: ignore
 
-    albums = serializers.ManyToManyField("albums.Album", related_name="tags")  # type: ignore
+    albums = serializers.ManyToManyField("albums.Album", related_name="tags", many=True)  # type: ignore
