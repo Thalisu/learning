114660edb987b3122b3e4f6c7e82d1166b48be02
Thalisu <thalisondmcc@gmail.com>
2024-09-16 13:09:27 -0300

    1:1 relation

--- python/bios/models.py
+++ python/bios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Bio(models.Model):
+    author = models.CharField(max_length=100)  # type: ignore
+    title = models.CharField(max_length=50)  # type: ignore
+    pulished_date = models.DateField(null=True)  # type: ignore
+
+    musician = models.OneToOneField(
+        "musicians.Musician", on_delete=models.CASCADE
+    )  # type: ignore
