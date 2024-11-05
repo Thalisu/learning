a5344424b19968483118adf00bb3c431d4345216
Thalisu <thalisondmcc@gmail.com>
2024-11-05 16:17:27 -0300

    Binary search implementation

--- Algorithms/kata-machine/src/day1/BinarySearchList.ts
+++ Algorithms/kata-machine/src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+export default function bs_list(
+    haystack: number[],
+    needle: number,
+    low = 0,
+    high = haystack.length,
+): boolean {
+    if (low >= high) return false;
 
+    const middle = Math.floor((low + high) / 2);
+
+    if (haystack[middle] === needle) {
+        return true;
+    } else if (haystack[middle] < needle) {
+        return bs_list(haystack, needle, middle + 1);
+    } else {
+        return bs_list(haystack, needle, low, middle);
+    }
 }
