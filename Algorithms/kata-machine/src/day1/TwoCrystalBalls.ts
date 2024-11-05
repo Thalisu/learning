c45b6294b219d5816ff8aa7e924cca64ab690bb9
Thalisu <thalisondmcc@gmail.com>
2024-11-05 18:45:55 -0300

    TwoCrystalBalls implementation

--- Algorithms/kata-machine/src/day1/TwoCrystalBalls.ts
+++ Algorithms/kata-machine/src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,13 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
 
+    for (let i = jump; i < breaks.length; i += jump) {
+        if (breaks[i]) {
+            for (let j = i - jump; j < breaks.length; ++j) {
+                if (breaks[j]) return j;
+            }
+        }
+    }
+
+    return -1;
 }
